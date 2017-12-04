961a218e95d1f6e40e4876c34ecf0a3939bd7eb3
Genar Trias Ortiz <genar@acs.li>
2017-12-04 21:28:34 +0100

    Feeding page with personal wiki

---
+++ Gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp')
+var removeCode = require('gulp-remove-code')
+
+gulp.task('clean', () => {
+  gulp.src('~/src/orgmode/**/*.md')
+    .pipe(removeCode({
+      commentStart: '<!--',
+      commentEnd: '-->',
+      production: true
+    }))
+  .pipe(gulp.dest('./source/'))
+})
+
+gulp.task('move files', () => {
+})
+
+gulp.task('default', ['clean'])
