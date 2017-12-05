103e86463b6dd920c42338cfe55137b7e83a8072
Genar Trias Ortiz <genar@acs.li>
2017-12-05 12:50:26 +0100

    Fixing md links

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp')
 var removeCode = require('gulp-remove-code')
+var replace = require('gulp-replace')
 
 gulp.task('clean', () => {
   gulp.src('/home/genar/src/orgmode/**/*.md')
+    .pipe(replace('.md', '.html'))
     .pipe(removeCode({
       commentStart: '<!--',
       commentEnd: '-->',
