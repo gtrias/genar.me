eb508f020e989f58bb2c3462385eaa02968730f0
Genar Trias Ortiz <genar@acs.li>
2017-12-05 16:15:45 +0100

    Allowing to define ignore tags

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,16 +1,14 @@
 var gulp = require('gulp')
-var removeCode = require('gulp-remove-code')
+var stripCode = require('gulp-strip-code')
 var replace = require('gulp-replace')
 
 gulp.task('clean', () => {
   gulp.src('/home/genar/src/orgmode/**/*.md')
     .pipe(replace('.md', '.html'))
-    .pipe(removeCode({
-      commentStart: '<!--',
-      commentEnd: '-->',
-      production: true
+    .pipe(stripCode({
+      pattern: /\[start_ignore\][\s\S]*?(?:\[end_ignore\].*)/g
     }))
-  .pipe(gulp.dest('./source/'))
+    .pipe(gulp.dest('./source/'))
 })
 
 gulp.task('move files', () => {
