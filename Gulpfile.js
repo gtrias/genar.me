d840d1602ba88248e5f040d438177eed81fc7715
Genar Trias Ortiz <genar@acs.li>
2017-12-05 16:47:02 +0100

    Fixing gulp replace

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,12 +1,16 @@
 var gulp = require('gulp')
 var stripCode = require('gulp-strip-code')
 var replace = require('gulp-replace')
+var gulpIgnore = require('gulp-ignore')
+
+var conditions = ['personal.md', 'regalo-xurri.md', 'jobs.md']
 
 gulp.task('clean', () => {
   gulp.src('/home/genar/src/orgmode/**/*.md')
+    .pipe(gulpIgnore.exclude(conditions))
     .pipe(replace('.md', '.html'))
     .pipe(stripCode({
-      pattern: /\[start_ignore\][\s\S]*?(?:\[end_ignore\].*)/g
+      pattern: /\[start_ignore\][\s\S]*?(?:\[end_ignore\].*\n+?)/g
     }))
     .pipe(gulp.dest('./source/'))
 })
