b1eee6e0f67e42c147505fa01fb73dac3b2c6749
Genar Trias Ortiz <genar@acs.li>
2017-12-07 12:19:18 +0100

    Fixing small things

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,12 +2,11 @@ var gulp = require('gulp')
 var stripCode = require('gulp-strip-code')
 var replace = require('gulp-replace')
 var gulpIgnore = require('gulp-ignore')
-
-var conditions = ['personal.md', 'regalo-xurri.md', 'jobs.md']
+var hexoIgnore = require('/home/genar/src/orgmode/hexoignore.json')
 
 gulp.task('clean', () => {
   gulp.src('/home/genar/src/orgmode/**/*.md')
-    .pipe(gulpIgnore.exclude(conditions))
+    .pipe(gulpIgnore.exclude(hexoIgnore.ignore))
     .pipe(replace('.md', '.html'))
     .pipe(stripCode({
       pattern: /\[start_ignore\][\s\S]*?(?:\[end_ignore\].*\n+?)/g
